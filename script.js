3a844c00ff1b1e5dbe7c7188cf18344d7219bf0e
Ariel <ariel.n.lopez@hotmail.com>
2022-12-12 19:26:19 -0300

    Nuevo proyecto

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
-alert("Bienvenido. Ingrese el accesorio que desea comprar")
-let ElijaAccesorio = Number(prompt("1-Zapatilla $21000 2-Buzo $8000 3-Remera $3500 4-Campera $15000"))
+const indumentaria = [
+  {nombre:"Pantalon", precio: 8500},
+  {nombre:"Campera", precio: 15000},
+  {nombre:"Remera", precio: 3500},
+  {nombre:"Zapatillas", precio: 22000},
+  {nombre:"Mochila", precio: 11000},
+];
 
-while (ElijaAccesorio !="CANCELAR") {
-  switch (ElijaAccesorio) {
-    case 1:
-      ElijaAccesorio = Number(alert("Usted ha agregado unas Zapatillas al carrito"))
-      
-      break;
+let carrito= []
 
-    case 2:
-      ElijaAccesorio = Number(alert("Usted ha agregado un Buzo al carrito"))
-        break;
+alert("Bienvenido a ANL indumentaria deportiva")
 
-    case 3:
-      ElijaAccesorio = Number(alert("Usted ha agregado una Remera al carrito"))
-          break;
+let seleccion = prompt("¿Usted quiere comprar algo? escriba si o no")
 
-     case 4:
-      ElijaAccesorio = Number(alert("Usted ha agregado una Campera al carrito"))
-       break;
-  
-    default:
-      break;
+while (seleccion !="si" && seleccion !="no") {
+  alert("Por favor ingrese una respuesta valida")
+  seleccion = prompt ("¿Usted quiere comprar algo? escriba si o no")
   }
   
-}
+  if (seleccion =="si") {
+    let stockIndumentaria = indumentaria.map((indumentaria)=> indumentaria.nombre + " " + "$"+ indumentaria.precio);
+    alert(stockIndumentaria.join ("//"))
+  }
+  else if (seleccion =="no") {
+    alert("Muchas gracias por habernos visitado")
+  }
+  while (seleccion ="si") {
+    let indumentaria = prompt ("Agrega a tu carrito la indumentaria que quieras")
+    
+    if (indumentaria == "Pantalon" || indumentaria == "Campera" ||indumentaria == "Remera" ||indumentaria == "Zapatillas" ||indumentaria == "Mochila" ){
+      switch (indumentaria) {
+        case "Pantalon":
+          precio = 8500;
+          break;
+          case "Campera":
+          precio = 15000;
+          break;
+          case "Remera":
+          precio = 3500;
+          break;
+          case "Zapatillas":
+          precio = 22000;
+          break;
+          case "Mochila":
+          precio = 11000;
+          break;
+          default:
+          break;
+      }
+    }
+    let unidades = parseInt(prompt("¿Cuantas unidades quiere llevar?"))
+    seleccion = prompt ("¿Quiere agregar otra indumentaria a su carrito?")
+    while (seleccion =="no") {
+      alert("Muchas gracias por habernos elejido")
+      carrito.push({indumentaria, unidades, precio})
+      console.log(carrito);
+    }
+  
+  }
+
+
+
+
+  
+
+
